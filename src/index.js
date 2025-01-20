de2458c8cb332cc15af69948a44909daffa7f736
Thang0226 <thang.nd0226@gmail.com>
2025-01-20 11:57:13 +0700

    can render time every 1 second

--- src/index.js
+++ src/index.js
@@ -22,13 +22,23 @@ const fruits = [
 ];
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-    <div>
-        <h1>List of fruits</h1>
-        <ul>
-            {fruits.map(item => (
-                <li>{item}</li>
-            ))}
-        </ul>
-    </div>
-);
+// root.render(
+//     <div>
+//         <h1>List of fruits</h1>
+//         <ul>
+//             {fruits.map(item => (
+//                 <li>{item}</li>
+//             ))}
+//         </ul>
+//     </div>
+// );
+const tick = () => {
+    root.render(
+        <div>
+            <h1>Hello, world!</h1>
+            <h2>It is {new Date().toLocaleTimeString()}.</h2>
+        </div>
+    );
+}
+setInterval(tick, 1000);
+
