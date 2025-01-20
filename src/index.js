045d7db0f1ee572dd671d294b535af5e8e76f5b8
Thang0226 <thang.nd0226@gmail.com>
2025-01-20 11:44:36 +0700

    can render list of fruits by JSX

--- src/index.js
+++ src/index.js
@@ -4,10 +4,31 @@ import ReactDOM from 'react-dom/client';
 const name = "Nguyen Thang";
 // React.createElement("h1", { style: { textAlign: "center" } }, name);
 
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(
+//     // React.createElement("h1", { style: { textAlign: "center" } }, name)
+//     <h1 style={{textAlign: "center"}}>
+//         {name}
+//     </h1>
+// );
+
+const fruits = [
+    "Apple",
+    "Banana",
+    "Orange",
+    "Apricot",
+    "Black rowan",
+    "Cranberry"
+];
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    // React.createElement("h1", { style: { textAlign: "center" } }, name)
-    <h1 style={{textAlign: "center"}}>
-        {name}
-    </h1>
+    <div>
+        <h1>List of fruits</h1>
+        <ul>
+            {fruits.map(item => (
+                <li>{item}</li>
+            ))}
+        </ul>
+    </div>
 );
